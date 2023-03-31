08ddb06c3b531e59e32e05262446b0bebff8ca2b
Joe W <whobbes22@gmail.com>
2023-03-31 16:02:04 -0700

    random color for each letter works

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,30 @@
+// Utility Logic
+
+//function that gets a random hex value - to be used with setAtrribute
+function randomColor(){
+  let c = "color: #";
+  for(let i = 0; i<6; i++){
+    const rando = Math.round(Math.random() *15);
+    if(rando === 10){
+      c+="A";
+    } else if(rando === 11){
+      c+="B";
+    } else if(rando === 12){
+      c+="C";
+    } else if(rando === 13){
+      c+="D";
+    } else if(rando === 14){
+      c+="E";
+    } else if(rando === 15){
+      c+="F"
+    } else{
+      c+=rando;
+    }
+  }
+  console.log(c);
+  return c;
+}
+
 // not UI logic
 
 function getInput(){
@@ -13,6 +40,7 @@ function getName(){
   }
 }
 
+// 2 paths to get to main logic that pass bool that will be used to reverse array if needed
 function transformReverse(event){
   event.preventDefault();
   transform(true);
@@ -41,35 +69,15 @@ function transform(isReversed){
       numArray.push(" "+i);
     }   
   }
+
+  // numArray                         - for words / whole sections
+  // numArray.join("").split("")      - for each character
+  numArray = numArray.join("").split("");
   if(isReversed){
     displayResults(numArray.reverse());
   } else {
     displayResults(numArray);
   }
-}
-
-function randomColor(){
-  let c = "color: #";
-  for(let i = 0; i<6; i++){
-    const rando = Math.round(Math.random() *15);
-    if(rando === 10){
-      c+="A";
-    } else if(rando === 11){
-      c+="B";
-    } else if(rando === 12){
-      c+="C";
-    } else if(rando === 13){
-      c+="D";
-    } else if(rando === 14){
-      c+="E";
-    } else if(rando === 15){
-      c+="F"
-    } else{
-      c+=rando;
-    }
-  }
-  console.log(c);
-  return c;
 }
 
 // UI logic
@@ -79,17 +87,27 @@ window.addEventListener("load", function(){
   const rev = document.querySelector("button#reversed")
   num.addEventListener("click", notTransformReverse);
   rev.addEventListener("click", transformReverse);
-
-
-  const sp1 = document.createElement("span");
-  sp1.append("hi");
-  let c = "color: #9901AD"
-  sp1.setAttribute("style", randomColor())
-  const targetResults = document.querySelector("#crazyIdea")
-  targetResults.append(sp1);
 });
 
 function displayResults(numArray){
-  document.querySelector("p#results").innerText = numArray;
+
+  document.querySelector("p#results").innerText ="";
+  document.querySelector("p#crazyIdea").innerText = "";
+
+  numArray.forEach(function (element){
+    console.log(element)
+
+    const sp1 = document.createElement("span");
+    sp1.append(element);
+    sp1.setAttribute("style", randomColor())
+    const targetResults = document.querySelector("#crazyIdea")
+    targetResults.append(sp1);
+
+  });
+
   
+  document.querySelector("p#results").append(numArray);
+  
+
+
 }
